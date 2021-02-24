1f5297111edde2b0b2b69ab6e8769d97867e0be5
Jelena Bajat <jelenabajat94@gmail.com>
2021-02-23 22:58:44 -0800

    FirstPart

---
+++ src/Pages/LocationPopupPage.java
@@ -0,0 +1,55 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LocationPopupPage extends BasicPage{
+	
+	public LocationPopupPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
+		super(driver, js, waiter);
+	}
+	
+	public WebElement getSelectLocation() {
+		return this.driver.findElement(By.id("locality_keyword"));	
+	}
+
+	public WebElement getClose() {
+		return this.driver.findElement(By.xpath("//*[@id=\"location-popup\"]/div/div/div/div/a"));
+	}
+	
+	public WebElement getKeyword(){
+		return this.driver.findElement(By.xpath("//*[@id='locality_keyword']"));
+	}
+	public WebElement getLocationItem(String locationName) {
+		return this.driver.findElement(By.xpath("//li/a[contains(text(), '" + locationName + "')]/.."));
+	}
+	
+	public WebElement getLocationInput() {
+		return this.driver.findElement(By.xpath("//*[@id='location_id']"));
+	}
+	
+	public WebElement getSubmit() {
+		return this.driver.findElement(By.xpath("//*[@name='btn_submit']"));
+	}
+
+	public WebElement PopUpSelectLocation() {
+		return this.driver.findElement(By.linkText("Select Location"));
+	}
+	
+	public void setLocation(String locationName) {
+		this.getKeyword().click();	
+		String location = this.getLocationItem(locationName).getAttribute("data-value");
+		js.executeScript("arguments[0].value = arguments[1]", this.getLocationInput(), location);
+		js.executeScript("arguments[0].click()", this.getSubmit());
+		
+	}
+	
+	public void closePopup() {
+		this.getClose().click();
+	}
+	
+}
---
+++ src/Pages/LoginPage.java
@@ -0,0 +1,54 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage extends BasicPage {
+
+	private String url = "/guest-user/login-form";
+	
+	public LoginPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
+		super(driver, js, waiter);
+	}
+	
+	public WebElement getLoginBtn() {
+		return this.driver.findElement(By.linkText("Login"));
+	}
+	
+	public WebElement getEmail() {
+		return this.driver.findElement(By.name("username"));
+	}
+	
+	public WebElement getPassword() {
+		return this.driver.findElement(By.name("password"));
+	}
+	
+	public WebElement getRememberMe() {
+		return this.driver.findElement(By.name("remember_me"));
+	}
+	
+	public WebElement getLoginBtnSecond() {
+		return this.driver.findElement(By.name("btn_submit"));
+	}
+	
+	public WebElement getFacebookLogin() {
+		return this.driver.findElement(By.linkText("FACEBOOK"));
+	}
+	
+	public WebElement getGoogleLogin() {
+		return this.driver.findElement(By.linkText("GOOGLE"));
+	}
+	
+	public WebElement getSignUpNow() {
+		return this.driver.findElement(By.linkText("Signup Now"));
+	}
+	
+	public void loginMember(String email, String password) {
+		this.getEmail().sendKeys(email);
+		this.getPassword().sendKeys(password);
+		this.getLoginBtnSecond().click();
+	}
+}
